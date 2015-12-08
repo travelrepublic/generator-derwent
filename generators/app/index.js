c55ef04fe2e869991ab91fd79476626624693213
mvisintin <mvisintin@travelrepublic.co.uk>
2015-12-08 15:18:08 +0000

    Generator | Changed default name

--- generators/app/index.js
+++ generators/app/index.js
@@ -23,7 +23,7 @@ module.exports = yeoman.generators.Base.extend({
       type: 'input',
       message: 'What is the block name?',
       name: 'app',
-      default: 'bobo'
+      default: 'derwentblock'
     }, function (answer) {
       appname = answer.app;
       done();
